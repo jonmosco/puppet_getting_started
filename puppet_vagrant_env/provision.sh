c021f7271de8fb728a0995caa6ac12b3878c2cd4
Jon Mosco <jonny.mosco@gmail.com>
2013-09-13 22:23:26 -0400

    initial commit of getting started with Puppet Presentation

---
+++ puppet_vagrant_env/provision.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+#
+# Set up the Master and Agent Nodes to talk with each other
+# Purpose: Learning Puppet
+#
+# Master:
+# - Edit /etc/hosts
+# - Turn off iptables
+# Node:
+# - Edit /etc/hosts
+# - Turn off iptables
+#
+# TODO
+# - turn if into case [DONE 7/22/13]
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
+HOSTS="192.168.33.10 puppet.forkedprocess.com puppet
+192.168.33.11 node.forkedprocess.com node"
+
+SITE=`cat <<-END
+node default {
+  include profile::base
+}
+END
+`
+
+# Get out hostname
+NAME=$( hostname -s )
+
+case $NAME in
+  puppet)
+    echo "$HOSTS" >> /etc/hosts
+
+    echo "Turning off Firewall and setting selinux to Permissive on our Puppet Master..."
+    service iptables stop > /dev/null && chkconfig iptables off > /dev/null 2>&1
+    #setenforce 0
+    echo "Done"
+
+    echo "Installing Puppet Master.."
+    rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm \
+      > /dev/null 2>&1
+    yum install puppet-server -y > /dev/null 2>&1
+    echo "Done"
+    echo "Building site manifest"
+    echo "$SITE" >> /etc/puppet/manifests/site.pp
+    echo "Done"
+
+    echo "Starting Puppet Master Service"
+    puppet resource service puppetmaster ensure=running enable=true > /dev/null 2>&1
+    master=$?
+    if [ $master -eq 0 ]; then
+      echo "Done"
+    fi
+    ;;
+  node)
+    echo "$HOSTS" >> /etc/hosts
+
+    echo "Turning off Firewall and setting selinux to Permissive on our Puppet Node..."
+    service iptables stop > /dev/null 2>&1 \
+      && chkconfig iptables off > /dev/null 2>&1
+    #setenforce 0 > /dev/null 2>&1
+    echo "Done"
+
+    echo "Starting Puppet Agent Service"
+    puppet resource service puppet ensure=running enable=true > /dev/null 2>&1
+    agent=$?
+    if [ $agent -eq 0 ]; then
+      echo "Done"
+    fi
+    ;;
+esac
